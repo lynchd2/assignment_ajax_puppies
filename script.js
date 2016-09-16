a2970a71a9d396f7755e2c3496b0d7ffba5e73ec
jsteenb2 <jonathansteenbergen@gmail.com>
2016-09-16 12:00:52 -0700

    added post request for submitting new dog

--- script.js
+++ script.js
@@ -11,9 +11,38 @@
       });
     };
 
+    var registerPuppy = function(callbackFunction){
+      var _$name = $('input[id="name"]').val();
+      var _$breed = $('input[name="breed"]').val();
+      $.ajax({
+        url: "https://ajax-puppies.herokuapp.com/puppies.json",
+        data: {
+          name: _$name,
+          breed: {
+            name: _$breed
+          }
+        },
+        // type: "POST",
+        contentType: 'Access-Control-Allow-Origin',
+        dataType: "json",
+        success: function(json){
+          console.log(json);
+        },
+        error: function(xhr, status, errorThrown){
+          console.log("Error: " + errorThrown);
+          console.log("Status: " + status);
+          console.log( xhr );
+        },
+        complete: function(xhr, status){
+          console.log("donezo");
+        }
+      });
+    };
+
     return {
-      getCurrentPuppies: getCurrentPuppies
-    }
+      getCurrentPuppies: getCurrentPuppies,
+      registerPuppy: registerPuppy
+    };
   })();
 
 
@@ -23,11 +52,15 @@ $(function() {
      puppiesAPI.getCurrentPuppies(function(data) {
       data.forEach(function(puppy) {
         $(".puppies-list").append("<p>" + puppy.name + "</p>");
-      })
-    })
+      });
+    });
   });
 
-  
+  $("input[type='submit']").on("click", function(e) {
+     e.preventDefault();
+     puppiesAPI.registerPuppy();
+  });
+
 });
 
 //  $("a").on("click", function(e){
