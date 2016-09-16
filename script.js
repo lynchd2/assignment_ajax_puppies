6d1a97d8673c05035566cdf28554e8b93ff325e0
Morgan Martin <morganm5201@gmail.com>
2016-09-16 18:43:54 -0400

    deletion

--- script.js
+++ script.js
@@ -1,90 +1,111 @@
+var puppiesAPI = (function() {
 
-  var puppiesAPI = (function() {
+  var _rootPath = "https://ajax-puppies.herokuapp.com/";
 
-    var _rootPath = "https://ajax-puppies.herokuapp.com/";
+  var getCurrentPuppies = function(callbackFunction) {
+    var _breedsPath = _rootPath + "puppies.json";
+    $.getJSON(_breedsPath, function(data) {
+      callbackFunction(data);
+    });
+  };
 
-    var getCurrentPuppies = function(callbackFunction) {
-      var _breedsPath = _rootPath + "breeds.json";
-      $.getJSON(_breedsPath, function(data) {
-        callbackFunction(data);
-      });
-    };
+  var adoptPuppy = function(id) {
+    var url = 'https://ajax-puppies.herokuapp.com/puppies/' + id + '.json';
+    $.ajax({
+      url: url,
+      type: "DELETE",
+      contentType: 'application/json',
+      dataType: "json",
+      success: function(puppy){
+        console.log(puppy);
+        removeDogItem(puppy.id);
+      },
+      error: function(xhr, status, errorThrown){
+        console.log("Error: " + errorThrown);
+        console.log("Status: " + status);
+        console.log( xhr );
+        $('body p').first().text('Error: ' + errorThrown);
+      },
+      complete: function(xhr, status){
+        console.log("donezo");
+      }
+    });
+  };
 
-    var registerPuppy = function(callbackFunction){
-      var _$name = $('input[id="name"]').val();
-      var _$breed = $('input[name="breed"]').val();
-      $.ajax({
-        url: "https://ajax-puppies.herokuapp.com/puppies.json",
-        data: JSON.stringify({
-          name: _$name,
-          breed_id: 2
-        }),
-        type: "POST",
-        contentType: 'application/json',
-        dataType: "json",
-        success: function(json){
-          console.log(json);
-        },
-        error: function(xhr, status, errorThrown){
-          console.log("Error: " + errorThrown);
-          console.log("Status: " + status);
-          console.log( xhr );
-        },
-        complete: function(xhr, status){
-          console.log("donezo");
-        }
-      });
-    };
+  var removeDogItem = function(id) {
+    $("[data-id='"+ id + "']").parent().remove();
+  };
 
-    return {
-      getCurrentPuppies: getCurrentPuppies,
-      registerPuppy: registerPuppy
-    };
-  })();
+  var registerPuppy = function(callbackFunction){
+    var _$name = $('input[id="name"]').val();
+    var _$breed = $('input[name="breed"]').val();
+    $.ajax({
+      url: "https://ajax-puppies.herokuapp.com/puppies.json",
+      data: JSON.stringify({
+        name: _$name,
+        breed_id: 2
+      }),
+      type: "POST",
+      contentType: 'application/json',
+      dataType: "json",
+      success: function(puppy){
+        console.log(puppy);
+        addDogItem(puppy);
+      },
+      error: function(xhr, status, errorThrown){
+        console.log("Error: " + errorThrown);
+        console.log("Status: " + status);
+        console.log( xhr );
+        $('body').prepend('<p>' + 'Error: ' + errorThrown + '</p>');
+        $('body p').addClass('error');
+      },
+      complete: function(xhr, status){
+        console.log("donezo");
+      }
+    });
+  };
+
+  var addDogItem = function(puppy) {
+    $(".puppies-list").prepend("<p>" + puppy.name + "</p>");
+    var adoptLink = $('<a>')
+          .attr('href', '#')
+          .text('Adopt')
+          .addClass('adopt-link')
+          .attr('data-id', puppy.id);
+    $('.puppies-list p').first().append(adoptLink);
+  };
+
+  return {
+    getCurrentPuppies: getCurrentPuppies,
+    registerPuppy: registerPuppy,
+    adoptPuppy: adoptPuppy,
+    addDogItem: addDogItem
+  };
+})();
 
 
 $(function() {
   $("a").on("click", function(e) {
-     e.preventDefault();
-     puppiesAPI.getCurrentPuppies(function(data) {
+    e.preventDefault();
+    $('.puppies-list p').remove();
+    puppiesAPI.getCurrentPuppies(function(data) {
+      data = data.reverse();
       data.forEach(function(puppy) {
-        $(".puppies-list").append("<p>" + puppy.name + "</p>");
+        puppiesAPI.addDogItem(puppy);
       });
     });
   });
 
   $("input[type='submit']").on("click", function(e) {
-     e.preventDefault();
-     puppiesAPI.registerPuppy() 
-     console.log(e)
+    e.preventDefault();
+    puppiesAPI.registerPuppy();
+  });
+
+  $('body').on('click', '.adopt-link', function(e) {
+    e.preventDefault();
+    var $target = $(e.target);
+    // console.log($target.data('id'));
+    puppiesAPI.adoptPuppy($target.data('id'));
   });
 
 });
-
-//  $("a").on("click", function(e){
-//   e.preventDefault();
-//   $.get("https://ajax-puppies.herokuapp.com/breeds.json", function(data) {
-//     $(".puppies-list").append(JSON.stringify(data));
-//   });
-//  })
-
-
-
-// })
-
-
-// var GeoIP = ( function(){
-//     // this private variable is the address you always hit up
-//     var _rootPath = "http://www.telize.com/geoip";
-
-//     // a method that gets an IP address
-//     // takes a callback function
-//     function selfIP(callback) {
-
-//       $.getJSON(_rootPath,
-//         function(json){
-//             // trigger the callback function
-//             callback(json.ip);
-//         });
-
-//     };
